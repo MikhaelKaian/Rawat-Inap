6366b3aee4d8dd6acb8028024549d8741db58830
Ega Satria <egasatria0112@gmail.com>
2023-01-17 18:19:28 +0700

    add pdf on pasien

--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-
+use App\Models\Pasien;
+use PDF;
 use App\Models\Laporan;
+
 class LaporanController extends Controller
 {
     public function index()
@@ -19,7 +21,7 @@ class LaporanController extends Controller
         {
             $pasiens = Pasien::all();
 
-            $pdf = PDF::loadview('print_pasiens', ['pasien'=>$pasiens]);
+            $pdf = PDF::loadview('print_pasiens', ['pasien'=>$pasiens])->setPaper('a4', 'landscape');;
             return $pdf->stream('data_pasien.pdf');
         }
 }
