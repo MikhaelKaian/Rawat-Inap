2d6f18888663107c87ac6524c6ed19151e3a268a
iqbal038 <iqbalfirdaus038@gmail.com>
2023-01-20 23:28:41 +0700

    fix timestamps migrations

---
+++ database/migrations/2023_01_20_162813_create_hasil_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHasilTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('hasil', function (Blueprint $table) {
+            $table->bigIncrements('id_hasil');
+
+            $table->bigInteger('id_dokter')->unsigned();
+            $table->foreign('id_dokter')->references('id_dokter')->on('dokter')->onDelete('cascade');
+            $table->bigInteger('id_pasien')->unsigned();
+            $table->foreign('id_pasien')->references('id_pasien')->on('pasien')->onDelete('cascade');
+            $table->string('kode_pasien');
+            $table->text('alamat');
+            $table->integer('lama_inap');
+            $table->enum('keterangan', ['rawat_inap', 'pulang']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('hasil');
+    }
+}
