e92863f033ccd95d87a209809ea7a43f3b77b751
iqbal038 <iqbalfirdaus038@gmail.com>
2023-01-21 17:12:25 +0700

    update table print_hasil

--- resources/views/print_hasil.blade.php
+++ resources/views/print_hasil.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <h1 class="text-center">Data Hasil Periksa</h1>
     <p class="text-center">Laporan Data Hasil Periksa</p>
-    <br/>
+    <br />
     <table id="table-data" class="table table-bordered">
     <thead>
                         <tr class="text-center">
